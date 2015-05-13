ea83247e4ef65f0b112c9eaed40c18e0fb5fef97
海豹 <007seadog@gmail.com>
2015-05-13 06:58:19 +0000

    Fixed indent

--- index.js
+++ index.js
@@ -19,27 +19,25 @@ console.log("#" + domain)
 toGoogleReq();
 
 function toGoogleReq() {
-	//console.log(curPage);
+	url="https://www.google.com.tw/search?q=%e5%b8%ab%e8%b3%87+OR+email+OR+e-mail+OR+%e4%bf%a1%e7%ae%b1+OR+%e9%83%b5%e4%bb%b6+site%3a" + domain + "+-filetype:pdf+-filetype:doc&start=" + curPage
+	console.log(url)
 	request({
-url: "https://www.google.com.tw/search?q=%e5%b8%ab%e8%b3%87+OR+email+OR+e-mail+OR+%e4%bf%a1%e7%ae%b1+OR+%e9%83%b5%e4%bb%b6+site%3a" + domain + "+-filetype:pdf+-filetype:doc&start=" + curPage,
-headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'},
-method: "GET"
-}, function(e,r,b) {
-curPage += 10;
-if (curPage <= pages) setTimeout(toGoogleReq, 10000);
-
-$ = cheerio.load(b);
-$('.srg>.g>.rc>.r>a').map(
-	function () {
-	//console.log($(this).attr('data-href') || $(this).attr('href'))
-	request({
-url: $(this).attr('data-href') || $(this).attr('href'),
-method: "GET"
-}, function(e,r,b) {
-//console.log(b)
-getMatches(b, mailregex, 1)
-})
-	}
-	)
-})
+		url: url,
+		headers: {'User-Agent': 'w3m/0.5.3+debian-15'},
+		method: "GET"
+	}, function(e,r,b) {
+		curPage += 10;
+		if (curPage <= pages) setTimeout(toGoogleReq, 10000);
+		$ = cheerio.load(b);
+		$('.srg>.g>.rc>.r>a').map(
+			function () {
+				request({
+					url: $(this).attr('data-href') || $(this).attr('href'),
+					method: "GET"
+				}, function(e,r,b) {
+					getMatches(b, mailregex, 1)
+				})
+			}
+		)
+	})
 }
